c5af8aa3431453c3a510dcf5bc7cef08bde54311
igor.bondezam <igorbfraca@gmail.com>
2025-04-12 10:36:45 -0300

    Configure: Adicionando profile de teste para executar os testes no CI

--- src/test/java/com/unicesumar/igor/rpg/RpgApplicationTests.java
+++ src/test/java/com/unicesumar/igor/rpg/RpgApplicationTests.java
@@ -2,8 +2,10 @@ package com.unicesumar.igor.rpg;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RpgApplicationTests {
 
 	@Test
